a75534215fb1e30eea00fe09c00203fbf5cd565d
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-06-11 16:33:39 +0200

    added a test file to check if everything works as expected

---
+++ test.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 11:37:04 by mamichal          #+#    #+#             */
+/*   Updated: 2024/06/11 00:01:25 by mamichal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/includ.h"
+#include "mlx_linux/mlx.h"
+#include <unistd.h>
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_img_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1000, 700, "Hello world");
+	img.img = mlx_new_image(mlx, 1000, 700);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	for (int i=0; i <=100; i++)
+	{
+		my_pixel_put(&img, 500 + i, 350, 0x00FF0000);
+		mlx_pixel_put(mlx, mlx_win, 500 + i, 350 + i, 0x00FF0000);
+	}
+	sleep(1);
+//	mlx_pixel_put(mlx, mlx_win, 5, 5, 0x00FF0000);
+//	my_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+	mlx_destroy_image(mlx, img.img);
+	mlx_destroy_window(mlx, mlx_win);
+}
