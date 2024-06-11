19a673284a8cfcec5be9857279f1ece556210b0b
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-06-11 16:33:17 +0200

    added my_pixel_put.c

---
+++ src/my_pixel_put.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   my_pixel_put.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 12:02:47 by mamichal          #+#    #+#             */
+/*   Updated: 2024/06/10 12:12:53 by mamichal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/includ.h"
+
+void	my_pixel_put(t_img_data *data, int x, int y, int color)
+{
+	char	*dst;
+	int		offset;
+
+	offset = ((y * data->line_length) + x * (data->bits_per_pixel / 8));
+	dst = data->addr + offset;
+	*(unsigned int*)dst = color;
+}
