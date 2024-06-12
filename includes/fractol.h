e0066151e4fe2760c791196bb47faeb3dd89b8d4
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-06-12 20:07:11 +0200

    changed ifndef name to match new file name in fractol.h

--- includes/fractol.h
+++ includes/fractol.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   includ.h                                           :+:      :+:    :+:   */
+/*   fractol.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 17:51:07 by mamichal          #+#    #+#             */
-/*   Updated: 2024/06/12 19:27:42 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/06/12 20:04:00 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef INCLUD_H
-# define INCLUD_H
+#ifndef FRACTOL_H
+# define FRACTOL_H
 
 // INCLUDES
 # include <stdlib.h>
@@ -21,10 +21,11 @@
 # include <stdbool.h>
 // TODO: DLEATE LATER AFTER ADDING PRINTF TO LIBFT
 //# include "../libft/includes/ft_printf.h"
+# include "../libft/libft.h"
 # include "../mlx_linux/mlx.h"
 
-#define WIDTH 500
-#define HIGHT 500
+# define WIDTH 500
+# define HIGHT 500
 
 typedef enum e_errors
 {
@@ -32,20 +33,33 @@ typedef enum e_errors
 	MALLOC_ERROR = 1
 }				t_errors;
 
-typedef struct	s_img_data
+typedef struct s_img_data
 {
 	void	*img;
 	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
+	int		bpp;
+	int		line_len;
 	int		endian;
 }				t_img_data;
 
-typedef struct	s_complex
+typedef struct s_complex
 {
 	double	real;
 	double	imaginary;
 }				t_complex;
+
+typedef struct s_mlx_data
+{
+	void		*mlx_conn;
+	void		*mlx_win;
+	t_img_data	img_data;
+
+}				t_mlx_data;
+
+typedef struct s_fractal
+{
+	char	*name;
+}				t_fractal;
 
 // src/my_put_pixel.c
 void	my_pixel_put(t_img_data *data, int x, int y, int color);
