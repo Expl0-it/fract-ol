c2264f712f3d586a8c692169e140912fb27ccab3
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-06-13 11:32:08 +0200

    added fractol_data_init to main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 19:38:09 by mamichal          #+#    #+#             */
-/*   Updated: 2024/06/12 20:31:18 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/06/13 11:24:38 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 int	main(int argc, char **argv)
 {
+	t_fractol_data	data;
+
 	if (2 == argc && !ft_strncmp(argv[1], "mandelbrot", 10)
 		|| 4 == argc && !ft_strncmp(argv[1], "julia", 5))
 	{
-		
+		data.fractal.p_name = argv[1];
+		fractol_data_init(&data);
 	}
 	else
 	{
