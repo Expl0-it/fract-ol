c3cf5b324a10fda2c4e83a67e1ece2959b7db20d
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-06-13 11:32:21 +0200

    corrected wrong variable name in my_pixel_put

--- src/my_pixel_put.c
+++ src/my_pixel_put.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 12:02:47 by mamichal          #+#    #+#             */
-/*   Updated: 2024/06/12 20:09:31 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/06/13 11:26:53 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ void	my_pixel_put(t_img_data *img, int x, int y, int color)
 	int		offset;
 
 	offset = ((y * img->line_len) + x * (img->bpp / 8));
-	dst = img->addr + offset;
+	dst = img->p_addr + offset;
 	*(unsigned int *)dst = color;
 }
