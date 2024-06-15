9b46a972a9560f5d2eceb2dde4c9444be95a99b1
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-06-15 22:57:25 +0200

    added fractal_utils to fractal

--- includes/fractol.h
+++ includes/fractol.h
@@ -91,9 +91,9 @@ typedef struct s_fractal_utils
 
 typedef struct s_fractal
 {
-	char					*p_name;
+	char			*p_name;
 
-	t_fractol_data_utils	utils;
+	t_fractal_utils	utils;
 }				t_fractal;
 
 typedef struct s_fractol_data
