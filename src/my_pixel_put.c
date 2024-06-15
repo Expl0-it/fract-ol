c2c6a1916b99a466ed6bb08464f77628d4b1f7d2
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-06-15 23:26:43 +0200

    trying second verion of my_pixel_put() cause of segfault

--- src/my_pixel_put.c
+++ src/my_pixel_put.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 12:02:47 by mamichal          #+#    #+#             */
-/*   Updated: 2024/06/13 11:26:53 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/06/15 23:25:46 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,17 @@ void	my_pixel_put(t_img_data *img, int x, int y, int color)
 	char	*dst;
 	int		offset;
 
-	offset = ((y * img->line_len) + x * (img->bpp / 8));
+	offset = ((y * img->line_len) + (x * (img->bpp / 8)));
 	dst = img->p_addr + offset;
 	*(unsigned int *)dst = color;
 }
+
+/*
+void	my_pixel_put(t_img_data *img, int x, int y, int color)
+{
+	int	offset;
+
+	offset = (y * img->line_len) + (x * (img->bpp / 8));
+	*(unsigned int *)(img->p_addr + offset) = color;
+}
+*/
