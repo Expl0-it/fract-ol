8e3cefd55d0ddff35f3eec4b8b20aedfaece2f93
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-06-15 22:53:35 +0200

    added color declarations to the header

--- includes/fractol.h
+++ includes/fractol.h
@@ -32,6 +32,24 @@
 # define WIDTH 800
 # define HEIGHT 800
 
+
+// NOTE: COLORS (stolen from chatgpt)
+# define BLACK       0x000000  // RGB(0, 0, 0)
+# define WHITE       0xFFFFFF  // RGB(255, 255, 255)
+# define RED         0xFF0000  // RGB(255, 0, 0)
+# define GREEN       0x00FF00  // RGB(0, 255, 0)
+# define BLUE        0x0000FF  // RGB(0, 0, 255)
+
+// Psychedelic colors
+# define MAGENTA_BURST   0xFF00FF
+# define LIME_SHOCK      0xCCFF00
+# define NEON_ORANGE     0xFF6600
+# define PSYCHEDELIC_PURPLE 0x660066
+# define AQUA_DREAM      0x33CCCC
+# define HOT_PINK        0xFF66B2
+# define ELECTRIC_BLUE   0x0066FF
+# define LAVA_RED        0xFF3300
+
 typedef enum e_errors
 {
 	OK = 0,
