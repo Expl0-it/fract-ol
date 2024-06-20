3c0b22a2642df305aaa349c209a2607ddf9f05fe
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-06-20 13:59:31 +0200

    added cases to read values for julia

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 19:38:09 by mamichal          #+#    #+#             */
-/*   Updated: 2024/06/15 23:00:51 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/06/20 13:13:55 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,11 @@ int	main(int argc, char **argv)
 		|| 4 == argc && !ft_strncmp(argv[1], "julia", 5))
 	{
 		data.fractal.p_name = argv[1];
+		if (!ft_strncmp(argv[1], "julia", 5))
+		{
+			data.fractal.julia_x = ft_atod(argv[2]);
+			data.fractal.julia_y = ft_atod(argv[3]);
+		}
 		fractol_data_init(&data);
 		fractal_render(&data);
 		mlx_loop(data.mlx.p_mlx_conn);
