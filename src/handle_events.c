f8307c7b8dfc3e5889377e7a12ab9f7d06f9df92
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-06-21 10:55:04 +0200

    handled moving with arrows and scrolling in proper direction

--- src/handle_events.c
+++ src/handle_events.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 21:23:43 by mamichal          #+#    #+#             */
-/*   Updated: 2024/06/16 23:14:54 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/06/20 12:27:37 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,13 @@ int	handle_key(int keysym, t_fractol_data *data)
 	if (keysym == XK_Escape)
 		handle_close(data);
 	else if (keysym == XK_Left)
-		data->events.shift_horizontal += (0.5 * data->events.zoom);
-	else if (keysym == XK_Right)
 		data->events.shift_horizontal -= (0.5 * data->events.zoom);
+	else if (keysym == XK_Right)
+		data->events.shift_horizontal += (0.5 * data->events.zoom);
 	else if (keysym == XK_Down)
-		data->events.shift_vertical += (0.5 * data->events.zoom);
-	else if (keysym == XK_Up)
 		data->events.shift_vertical -= (0.5 * data->events.zoom);
+	else if (keysym == XK_Up)
+		data->events.shift_vertical += (0.5 * data->events.zoom);
 	else if (keysym == XK_plus)
 		data->fractal.utils.iterations_number += 10;
 	else if (keysym == XK_minus)
@@ -43,9 +43,9 @@ int	handle_key(int keysym, t_fractol_data *data)
 
 int	handle_mouse(int button, int horizontal, int vertical, t_fractol_data *data)
 {
-	if (Button5 == button)
+	if (Button4 == button)
 		data->events.zoom *= 0.95;
-	else if (Button4 == button)
+	else if (Button5 == button)
 		data->events.zoom *= 1.05;
 	fractal_render(data);
 	return (0);
